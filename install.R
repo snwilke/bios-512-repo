9c9dedde7e88186e08eba3fc173bee5ec70fb687
Sara Wilke <snwilke@unc.edu>
2025-09-07 23:14:11 +0000

    Restore files after accidental deletion

---
+++ install.R
@@ -0,0 +1,7 @@
+install.packages("readr")
+
+install.packages("tidyverse", dependencies = TRUE)
+install.packages("rmarkdown", dependencies = TRUE)
+install.packages("IRkernel", dependencies = TRUE)
+
+IRkernel::installspec(user = FALSE)
