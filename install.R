f31304e34162515a77ef60ac1c6f967a28c1c47c
snwilke <snwilke@unc.edu>
2025-09-15 04:20:37 +0000

    installing skimr

--- install.R
+++ install.R
@@ -3,5 +3,6 @@ install.packages("readr")
 install.packages("tidyverse", dependencies = TRUE)
 install.packages("rmarkdown", dependencies = TRUE)
 install.packages("IRkernel", dependencies = TRUE)
+install.packages("skimr", dependencies = TRUE)
 
 IRkernel::installspec(user = FALSE)
